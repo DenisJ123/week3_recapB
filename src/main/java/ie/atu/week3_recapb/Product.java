5e85f947cb22c8c31f326a61b334899556602003
denis <g00403126@atu.ie>
2024-10-04 14:38:02 +0100

    Product

---
+++ src/main/java/ie/atu/week3_recapb/Product.java
@@ -0,0 +1,21 @@
+package ie.atu.week3_recapb;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @NotBlank(message = "Id cannot be blank")
+    private String id;
+    @NotBlank(message = "Name cannot be null")
+    private String name;
+    @Positive(message = "Price cannot be negative")
+    private double price;
+
+
+}
