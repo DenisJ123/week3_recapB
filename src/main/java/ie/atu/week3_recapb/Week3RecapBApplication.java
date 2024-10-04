e3a6b7c033f71e427e68a39791f359a250b80bf5
denis <g00403126@atu.ie>
2024-10-04 14:29:02 +0100

    start

---
+++ src/main/java/ie/atu/week3_recapb/Week3RecapBApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3_recapb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3RecapBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3RecapBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3_recapb/Week3RecapBApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3_recapb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3RecapBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
